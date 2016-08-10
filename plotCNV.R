a93a057a6998d9a95cb1a03e062c08f428b8cb09
mikblack <blackmail@gmail.com>
2016-08-10 14:42:50 +1200

    Added existing lab files

---
+++ plotCNV.R
@@ -0,0 +1,56 @@
+plotCNV <- function(data,sample,gene,...,truncate=FALSE){
+  
+  pop<-rep(c("CEU","YRI","CHB"),c(99,109,103))
+  names(pop)<-c("NA06984","NA06985","NA06986","NA06989","NA06994","NA07000","NA07037","NA07048","NA07051","NA07056","NA07347","NA07357","NA10847","NA10851","NA11829","NA11830","NA11831","NA11832","NA11840","NA11843","NA11881","NA11892","NA11893","NA11894","NA11918","NA11919","NA11920","NA11930","NA11931","NA11932","NA11933","NA11992","NA11994","NA11995","NA12003","NA12004","NA12005","NA12006","NA12043","NA12044","NA12045","NA12046","NA12058","NA12144","NA12154","NA12155","NA12156","NA12234","NA12249","NA12272","NA12273","NA12275","NA12282","NA12283","NA12286","NA12287","NA12340","NA12341","NA12342","NA12347","NA12348","NA12383","NA12399","NA12400","NA12413","NA12414","NA12489","NA12546","NA12716","NA12717","NA12718","NA12748","NA12749","NA12750","NA12751","NA12760","NA12761","NA12762","NA12763","NA12775","NA12776","NA12777","NA12778","NA12812","NA12813","NA12814","NA12815","NA12827","NA12828","NA12829","NA12830","NA12842","NA12843","NA12872","NA12873","NA12874","NA12878","NA12889","NA12890","NA18486","NA18488","NA18489","NA18498","NA18499","NA18501","NA18502","NA18504","NA18505","NA18507","NA18508","NA18510","NA18511","NA18516","NA18517","NA18519","NA18520","NA18522","NA18523","NA18853","NA18856","NA18858","NA18861","NA18864","NA18865","NA18867","NA18868","NA18870","NA18871","NA18873","NA18874","NA18876","NA18877","NA18878","NA18879","NA18881","NA18907","NA18908","NA18909","NA18910","NA18912","NA18915","NA18916","NA18917","NA18923","NA18924","NA18933","NA18934","NA19092","NA19093","NA19095","NA19096","NA19098","NA19099","NA19102","NA19107","NA19108","NA19113","NA19114","NA19116","NA19117","NA19118","NA19119","NA19121","NA19129","NA19130","NA19131","NA19137","NA19138","NA19141","NA19143","NA19144","NA19146","NA19147","NA19149","NA19152","NA19153","NA19159","NA19160","NA19171","NA19172","NA19175","NA19184","NA19185","NA19189","NA19190","NA19197","NA19198","NA19200","NA19201","NA19204","NA19206","NA19207","NA19209","NA19210","NA19213","NA19214","NA19222","NA19223","NA19225","NA19235","NA19236","NA19238","NA19239","NA19240","NA19247","NA19248","NA19256","NA19257","NA18525","NA18526","NA18528","NA18530","NA18531","NA18532","NA18533","NA18534","NA18535","NA18536","NA18537","NA18538","NA18539","NA18541","NA18542","NA18543","NA18544","NA18545","NA18546","NA18547","NA18548","NA18549","NA18550","NA18552","NA18553","NA18555","NA18557","NA18558","NA18559","NA18560","NA18561","NA18562","NA18563","NA18564","NA18565","NA18566","NA18567","NA18570","NA18571","NA18572","NA18573","NA18574","NA18577","NA18579","NA18582","NA18591","NA18592","NA18593","NA18595","NA18596","NA18597","NA18599","NA18602","NA18603","NA18605","NA18606","NA18608","NA18609","NA18610","NA18611","NA18612","NA18613","NA18614","NA18615","NA18616","NA18617","NA18618","NA18619","NA18620","NA18621","NA18622","NA18623","NA18624","NA18625","NA18626","NA18627","NA18628","NA18629","NA18630","NA18631","NA18632","NA18633","NA18634","NA18635","NA18636","NA18637","NA18638","NA18639","NA18640","NA18641","NA18642","NA18643","NA18644","NA18645","NA18646","NA18647","NA18648","NA18740","NA18745","NA18747","NA18748","NA18749","NA18757")
+  
+  if(toupper(gene)=="FCGR"){
+    ch<-1; st<-161300000; en<-161800000; 
+    sz<-1000; num<-(en-st)/sz
+    genes<-data.frame(fcgr3b=c(161592990,161601158), 
+                      fcgr2b=c(161632940,161647951), 
+                      fcgr2c=c(161551129,161570031), 
+                      fcgr2a=c(161475205,161489359),
+                      fcgr3a=c(161511553,161520413))
+    gn<-c("3b","2b","2c","2a","3a")
+  }
+  if(toupper(gene)=="IRGM"){
+    ch<-5; st<-150124000; en<-150324000
+    sz<-500; num<-(en-st)/sz
+    genes<-data.frame(irgm=c(150226085,150228231))
+    gn<-"IRGM"
+  }
+  if(toupper(gene)=="AMY1A"){
+    ch<-1; st<-104125000; en<-104375000
+    sz<-500; num<-(en-st)/sz
+    genes<-data.frame(amy1a.1=c(104198141,104207173),
+                      amy1a.2=c(104230040,104239073),
+                      amy1a.3=c(104292279,104301311))
+    gn<-c("AMY1A","AMY1A","AMY1A")
+  }
+  if(!(toupper(gene)%in%c("FCGR","IRGM","AMY1A")))
+    stop("Gene needs to be one of: FCGR, IRGM, AMY1A")
+  
+  # Get data
+  aa<-match(sample,colnames(data))
+  if(is.na(aa)) stop(paste("Can't find sample",sample,"in the data"))
+  cnt<-data[,sample]
+  
+  # Set up for plot
+  yl<-c(0.9,1.1)*range(cnt)
+  if(truncate & gene=="FCGR") yl<-c(0.9,1.1)*range(cnt[-c(1:169)])
+  if(truncate & gene!="FCGR")
+    stop("The 'truncate' option cn only be used with the FCR region")
+  plot(seq(st,en,l=num),cnt,type='l',col='white',
+       xlab=paste('Chromosome', ch ,'Position (HG19/GRCh37)'),
+       ylab='Read Count',main=paste(gene,": ",sample,", ",
+                                    pop[names(pop)==sample],sep=''),
+       ylim=yl/c(0.9,1.1))
+  
+  # Add gene names
+  for(k in 1:ncol(genes)) rect(genes[1,k],yl[1],genes[2,k],yl
+                               [2],col=gray(0.8),border=NA)
+  for(k in 1:ncol(genes)) text(mean(genes[,k]),0.9*yl[2],gn[k],cex=0.75)
+  
+  # Add count data
+  lines(seq(st,en,l=num),cnt,type='l')  
+} 
